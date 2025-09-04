4518922099a5b342bc82bce4a6861f5ab4825b86
duong <minhduong5534@gmail.com>
2025-09-04 11:28:03 +0700

    .

---
+++ MODELS/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace Webhook_Message.Models // Thay bằng namespace của project
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string SenderId { get; set; }
+        public string Content { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
