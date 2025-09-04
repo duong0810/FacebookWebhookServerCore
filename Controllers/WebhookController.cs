5bd66289177d64ff2c696f8d68d675fb536d3dd4
duong <minhduong5534@gmail.com>
2025-09-04 09:37:03 +0700

    Ignore .vs directory and other temporary files

---
+++ Controllers/WebhookController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webhook_Message.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WebhookController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Tạm thời chỉ trả 200 OK để test deploy
+            return Ok("Webhook is running");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            // Tạm thời chỉ nhận request để test
+            return Ok();
+        }
+    }
+}
