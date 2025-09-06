b34a31abb316a5938023b1b1744543bec4c9b30d
duong <minhduong5534@gmail.com>
2025-09-06 08:52:34 +0700

    .

--- Program.cs
+++ Program.cs
@@ -4,18 +4,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-<<<<<<< HEAD
 // Thêm logging
 builder.Services.AddLogging(logging =>
 {
     logging.AddConsole(); // Ghi log ra console (Render sẽ hiển thị)
     logging.AddDebug();  // Tùy chọn, nếu cần debug cục bộ
-=======
-// Đặt cấu hình Kestrel ở đây, trước khi build app
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(int.Parse(Environment.GetEnvironmentVariable("PORT") ?? "5000"));
->>>>>>> edf9e186ef26d4a3328a3f907289256cbcde6e3d
 });
 
 var app = builder.Build();
@@ -30,5 +23,10 @@ if (app.Environment.IsDevelopment())
 app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(int.Parse(Environment.GetEnvironmentVariable("PORT") ?? "5000"));
+});
 
 app.Run();
