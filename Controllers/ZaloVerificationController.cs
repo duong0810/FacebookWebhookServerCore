fea4d475172c4706ba10a3133e360c73d3eedd64
duong <minhduong5534@gmail.com>
2025-09-11 16:08:25 +0700

    .

--- Controllers/ZaloVerificationController.cs
+++ Controllers/ZaloVerificationController.cs
@@ -2,13 +2,12 @@
 
 namespace FacebookWebhookServerCore.Controllers
 {
-    public class ZaloVerificationController : Controller
+    [ApiController]
+    public class ZaloVerificationController : ControllerBase
     {
-        [Route("zalo_verifierQE2wTxFh3bDmdBm9-hbk1YYismB4sW1TD3a.html")]
-        [HttpGet]
+        [HttpGet("/zalo_verifierQE2wTxFh3bDmdBm9-hbk1YYismB4sW1TD3a.html")]
         public ContentResult ZaloDomainVerification()
         {
-            // Trả về chính xác mã xác thực không có HTML tags
             return Content("QE2wTxFh3bDmdBm9-hbk1YYismB4sW1TD3a", "text/plain");
         }
     }
