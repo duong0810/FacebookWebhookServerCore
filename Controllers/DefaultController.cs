c0d0c986191adb70cf77ddd91d07f1135324eb29
duong <minhduong5534@gmail.com>
2025-09-12 10:08:21 +0700

    .

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -11,5 +11,11 @@ namespace FacebookWebhookServerCore.Controllers
             return Content("<h1>Server is running</h1>", "text/html");
         }
 
+        [Route("/zalo_verifierQE2wTxFh3bDmdBm9-hbk1YYismB4sW1TD3a.html")]
+        [HttpGet]
+        public ContentResult ZaloDomainVerification()
+        {
+            return Content("QE2wTxFh3bDmdBm9-hbk1YYismB4sW1TD3a", "text/plain");
+        }
     }
 }
