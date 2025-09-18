e3df693d80cf693e97bd2b2a30f1aa9b9a83d510
duong <minhduong5534@gmail.com>
2025-09-18 15:50:16 +0700

    .

--- Controllers/ZaloWebhookController.cs
+++ Controllers/ZaloWebhookController.cs
@@ -274,7 +274,6 @@ namespace FacebookWebhookServerCore.Controllers
                 string url = "https://openapi.zalo.me/v3.0/oa/message/cs";
                 var messagePayload = new
                 {
-                    text = "Hình ảnh từ OA", // Thay bằng nội dung hợp lệ
                     attachments = new[]
                     {
                         new
