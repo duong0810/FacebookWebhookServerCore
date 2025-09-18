a4503f0934a70810cf4846da6b716158951ab55f
duong <minhduong5534@gmail.com>
2025-09-18 17:27:59 +0700

    .

--- MODELS/ZaloMessageRequest.cs
+++ MODELS/ZaloMessageRequest.cs
@@ -2,7 +2,9 @@
 {
     public class ZaloMessageRequest
     {
-        public string RecipientId { get; set; }
+        public string RecipientId { get; set; } // Dùng cho flow tư vấn OA
         public string Message { get; set; }
+        public string AnonymousId { get; set; } // Dùng cho flow ẩn danh
+        public string ConversationId { get; set; } // Dùng cho flow ẩn danh
     }
 }
