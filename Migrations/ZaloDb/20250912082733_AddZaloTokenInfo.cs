e5b9b2430852e28fe6829182eab7071cb8e3e177
duong <minhduong5534@gmail.com>
2025-09-22 10:52:16 +0700

    .

--- Migrations/ZaloDb/20250912082733_AddZaloTokenInfo.cs
+++ Migrations/ZaloDb/20250912082733_AddZaloTokenInfo.cs
@@ -10,13 +10,27 @@ namespace Webhook_Message.Migrations.ZaloDb
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.CreateTable(
+                name: "ZaloTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AccessToken = table.Column<string>(type: "TEXT", nullable: true),
+                    RefreshToken = table.Column<string>(type: "TEXT", nullable: true),
+                    ExpireAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ZaloTokens", x => x.Id);
+                });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropTable(
+                name: "ZaloTokens");
         }
     }
 }
